43f4eb909f7e85238c0d051cf98ff62e9fd9f49c
Sida <sliu1@uvm.edu>
2019-08-28 13:40:40 -0400

    1

--- pygameWindow.py
+++ pygameWindow.py
@@ -7,6 +7,7 @@ class PYGAME_WINDOW:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((constants.pygameWindowWidth, constants.pygameWindowDepth))
+        self.last_status = ''
 
     def Prepare(self):
         for event in pygame.event.get():
@@ -14,9 +15,27 @@ class PYGAME_WINDOW:
         white = 255,255,255
         self.screen.fill(white)
 
+    def Fill(self, color):
+        self.screen.fill(color)
+
     def Reveal(self):
         pygame.display.update()
 
     def DrawBlackCircles(self, x, y):
         black = 0,0,0
-        pygame.draw.circle(self.screen, black, (x,y), constants.pygameCircleRadius)
+        pygame.draw.circle(self.screen, black, (int(x),int(y)), constants.pygameCircleRadius)
+
+    def Print(self, status):
+        font = pygame.font.SysFont("consolas", 16)
+        text = font.render(str(status), True, (255,0,0))
+        self.screen.blit(text, (5,constants.pygameWindowDepth-text.get_height()))
+
+
+if __name__ == "__main__":
+    w = PYGAME_WINDOW()
+    while(True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+        w.Prepare()
+        w.Print("test")
+        w.Reveal()
